c8173e7e23fc452cebf7c48db74db1475b2fae70
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-12 19:44:50 +0300

    с

--- src/routes/catalog/+page.svelte
+++ src/routes/catalog/+page.svelte
@@ -68,39 +68,39 @@
                             <th scope="col" class="px-4 py-3">
                                 <span class="sr-only">Expand/Collapse Row</span>
                             </th>
-                            <th scope="col" class="px-4 py-3 min-w-[14rem]">Product</th>
+                            <th scope="col" class="px-4 py-3 min-w-[14rem]">Продукт</th>
                             <th scope="col" class="px-4 py-3 min-w-[10rem]">
-                                Category
+                                Категория
                                 <svg class="h-4 w-4 ml-1 inline-block" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
                                 </svg>
                             </th>
                             <th scope="col" class="px-4 py-3 min-w-[6rem]">
-                                Brand
+                                Бренд
                                 <svg class="h-4 w-4 ml-1 inline-block" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
                                 </svg>
                             </th>
                             <th scope="col" class="px-4 py-3 min-w-[6rem]">
-                                Price
+                                Добавлено
                                 <svg class="h-4 w-4 ml-1 inline-block" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
                                 </svg>
                             </th>
                             <th scope="col" class="px-4 py-3 min-w-[6rem]">
-                                Stock
+                                Изменено
                                 <svg class="h-4 w-4 ml-1 inline-block" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
                                 </svg>
                             </th>
-                            <th scope="col" class="px-4 py-3 min-w-[12rem]">
+                            <!-- <th scope="col" class="px-4 py-3 min-w-[12rem]">
                                 Total Sales
                                 <svg class="h-4 w-4 ml-1 inline-block" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
                                 </svg>
-                            </th>
+                            </th> -->
                             <th scope="col" class="px-4 py-3 min-w-[7rem]">
-                                Status
+                                Статус
                                 <svg class="h-4 w-4 ml-1 inline-block" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" />
                                 </svg>
@@ -128,12 +128,12 @@
                             <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Apple</td>
                             <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">$2999</td>
                             <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">200</td>
-                            <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">245</td>
+                            <!-- <td class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">245</td> -->
                             <td class="px-4 py-3 whitespace-nowrap">
                                 <div class="w-fit bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">Active</div>
                             </td>
                         </tr>
-                        <tr class="hidden flex-1 overflow-x-auto w-full" id="table-column-body-0" aria-labelledby="table-column-header-0">
+                        <tr class=" flex-1 overflow-x-auto w-full" id="table-column-body-0" aria-labelledby="table-column-header-0">
                             <td class="p-4 border-b" colspan="9">
                                 <div class="grid grid-cols-4 gap-4 mb-4">
                                     <div class="relative p-2 bg-gray-100 rounded-lg sm:w-full h-32 sm:h-36 flex items-center justify-center">
@@ -141,21 +141,21 @@
                                     </div>
                                 </div>
                                 <div>
-                                    <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Details</h6>
+                                    <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Описание</h6>
                                     <div class="text-base text-gray-500 max-w-screen-md">Standard glass, 3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, 16GB 2666MHz DDR4 memory, Radeon Pro 5500 XT with 8GB of GDDR6 memory, 256GB SSD storage, Gigabit Ethernet, Magic Mouse 2, Magic Keyboard - US.</div>
                                 </div>
                                 <div class="grid grid-cols-4 gap-4 mt-4">
                                     <div class="relative p-3 bg-gray-100 rounded-lg flex flex-col items-start justify-between">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Product State</h6>
+                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Статус</h6>
                                         <div class="bg-primary-100 text-primary-800 text-xs font-medium px-2.5 py-0.5 rounded-md flex items-center">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 mr-1" viewbox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                                             </svg>
-                                            New
+                                            Активно
                                         </div>
                                     </div>
                                     <div class="relative p-3 bg-gray-100 rounded-lg flex flex-col justify-between">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Shipping</h6>
+                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Категория</h6>
                                         <div class="flex items-center text-gray-500">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 mr-1" viewbox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                                 <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
@@ -164,7 +164,7 @@
                                         </div>
                                     </div>
                                     <div class="relative p-3 bg-gray-100 rounded-lg">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Colors</h6>
+                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Теги</h6>
                                         <div class="flex items-center space-x-2">
                                             <div class="rounded-full h-6 w-6 bg-purple-600"></div>
                                             <div class="rounded-full h-6 w-6 bg-indigo-400"></div>
@@ -175,40 +175,25 @@
                                         </div>
                                     </div>
                                     <div class="relative p-3 bg-gray-100 rounded-lg">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Brand</h6>
+                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Бренд</h6>
                                         <div class="flex items-center text-gray-500">Apple</div>
                                     </div>
-                                    <div class="relative p-3 bg-gray-100 rounded-lg">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Sold by</h6>
-                                        <div class="flex items-center text-gray-500">Flowbite</div>
-                                    </div>
-                                    <div class="relative p-3 bg-gray-100 rounded-lg">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Ships from</h6>
-                                        <div class="flex items-center text-gray-500">Flowbite</div>
-                                    </div>
-                                    <div class="relative p-3 bg-gray-100 rounded-lg">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Dimensions (cm)</h6>
-                                        <div class="flex items-center text-gray-500">105 x 15 x 23</div>
-                                    </div>
-                                    <div class="relative p-3 bg-gray-100 rounded-lg">
-                                        <h6 class="mb-2 text-base leading-none font-medium text-gray-900">Item weight</h6>
-                                        <div class="flex items-center text-gray-500">12kg</div>
-                                    </div>
+                                    
                                 </div>
                                 <div class="flex items-center space-x-3 mt-4">
-                                    <button type="button" class="py-2 px-3 flex items-center text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">
+                                    <button type="button" class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-800 bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewbox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
                                             <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
                                         </svg>
-                                        Edit
+                                        Применить
                                     </button>
-                                    <button type="button" class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Preview</button>
+                                    <button type="button" class="py-2 px-3 flex items-center text-sm font-medium text-center text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Вернуть </button>
                                     <button type="button" class="py-2 px-3 flex items-center text-sm font-medium text-center text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 rounded-lg">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1" viewbox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                                         </svg>
-                                        Delete
+                                        Удалить
                                     </button>
                                 </div>
                             </td>
