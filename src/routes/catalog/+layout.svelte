1028e05ef531f603b3004b4ac86049210e608705
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-13 20:45:43 +0300

    c

--- src/routes/catalog/+layout.svelte
+++ src/routes/catalog/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	/** @type {{ data: import('./$types').LayoutData, children: import('svelte').Snippet }} */
 	let { children, data } = $props();
-	$inspect(data);
+
+    let currentSubCatalogIndex = $state(null)
+
+
+	$inspect(currentSubCatalogIndex);
 </script>
 
 <section class="bg-gray-50 py-3 sm:py-5">
@@ -143,28 +147,18 @@
 								</svg>
 							</button>
 
-							<!--
-                            Select popover, show/hide based on select state.
-                      
-                            Entering: ""
-                              From: ""
-                              To: ""
-                            Leaving: "transition ease-in duration-100"
-                              From: "opacity-100"
-                              To: "opacity-0"
-                          -->
-							<ul
+							<div
 								class=" absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
 								tabindex="-1"
 								role="listbox"
 								aria-labelledby="listbox-label"
 								aria-activedescendant="listbox-option-3"
 							>
-								{#each data.req.fullcatalog as item}
-									<li
+								{#each data.req.fullcatalog as item, i}
+									<button type="button"
+                                        onclick={() => currentSubCatalogIndex = i}
 										class="relative cursor-pointer select-none py-2 pl-3 pr-9 text-gray-900 hover:bg-gray-100"
 										id="listbox-option-0"
-										role="option"
 									>
 										<div class="flex items-center">
 											<!-- Online: "bg-green-400 forced-colors:bg-[Highlight]", Not Online: "bg-gray-200" -->
@@ -174,29 +168,84 @@
 											></span>
 											<!-- Selected: "font-semibold", Not Selected: "font-normal" -->
 											<span class="ml-3 block truncate font-normal">
-												{item.value}
+												{item.value}-{i}
 												<span class="sr-only"> is online</span>
 											</span>
 										</div>
-									</li>
+									</button>
 								{/each}
-							</ul>
+							</div>
 						</div>
 					</div>
 
+                    {#if currentSubCatalogIndex}
+
+                    <div>
+						<!-- <label id="listbox-label" class="block text-sm/6 font-medium text-gray-900">Каталог</label> -->
+						<div class="relative mt-2">
+							<button
+								type="button"
+								class="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 text-left text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-green-600 sm:text-sm/6"
+								aria-haspopup="listbox"
+								aria-expanded="true"
+								aria-labelledby="listbox-label"
+							>
+								<span class="col-start-1 row-start-1 flex items-center gap-3 pr-6">
+									<span
+										aria-label="Online"
+										class="inline-block size-2 shrink-0 rounded-full border border-transparent"
+									></span>
+									<span class="block truncate">Рубрика</span>
+								</span>
+								<svg
+									class="col-start-1 row-start-1 size-5 self-center justify-self-end text-green-500 sm:size-4"
+									viewBox="0 0 16 16"
+									fill="currentColor"
+									aria-hidden="true"
+									data-slot="icon"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M5.22 10.22a.75.75 0 0 1 1.06 0L8 11.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-2.25 2.25a.75.75 0 0 1-1.06 0l-2.25-2.25a.75.75 0 0 1 0-1.06ZM10.78 5.78a.75.75 0 0 1-1.06 0L8 4.06 6.28 5.78a.75.75 0 0 1-1.06-1.06l2.25-2.25a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06Z"
+										clip-rule="evenodd"
+									/>
+								</svg>
+							</button>
+
+							<div
+								class=" absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
+								tabindex="-1"
+								role="listbox"
+								aria-labelledby="listbox-label"
+								aria-activedescendant="listbox-option-3"
+							>
+								{#each data.req.fullcatalog[currentSubCatalogIndex].rubric as item, i}
+									<button type="button"
+										class="relative cursor-pointer select-none py-2 pl-3 pr-9 text-gray-900 hover:bg-gray-100"
+										id="listbox-option-0"
+									>
+										<div class="flex items-center">
+											<!-- Online: "bg-green-400 forced-colors:bg-[Highlight]", Not Online: "bg-gray-200" -->
+											<span
+												class="inline-block size-2 shrink-0 rounded-full border border-transparent bg-green-400"
+												aria-hidden="true"
+											></span>
+											<!-- Selected: "font-semibold", Not Selected: "font-normal" -->
+											<span class="ml-3 block truncate font-normal">
+												{item.value}-{i}
+												<span class="sr-only"> is online</span>
+											</span>
+										</div>
+									</button>
+								{/each}
+                            </div>
+						</div>
+					</div>
+                    {/if}
+
+
+	
 					<!-- <div class="w-full">
-						<label for="catalog" class="sr-only">Каталог</label>
-						<select
-							id="catalog"
-							class="peer block w-full appearance-none border-0 border-b-2 border-gray-200 bg-transparent px-0 py-2.5 text-sm text-gray-500 focus:border-gray-200 focus:outline-none focus:ring-0"
-						>
-							<option selected disabled>Каталог</option>
-							{#each data.req.fullcatalog as item}
-								<option value={item.slug}>{item.value}</option>
-							{/each}
-						</select>
-					</div> -->
-					<div class="w-full">
 						<label for="price" class="sr-only">Рубрика</label><select
 							id="price"
 							class="peer block w-full appearance-none border-0 border-b-2 border-gray-200 bg-transparent px-0 py-2.5 text-sm text-gray-500 focus:border-gray-200 focus:outline-none focus:ring-0"
@@ -206,7 +255,7 @@
 								>$ 1001+</option
 							></select
 						>
-					</div>
+					</div> -->
 					<div class="w-full">
 						<label for="category" class="sr-only">Категория</label><select
 							id="category"
