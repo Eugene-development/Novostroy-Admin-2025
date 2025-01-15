28d072a7b36513853c3154d14ad32f6c222120ef
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-15 22:13:54 +0300

    c

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,49 @@
 /** @satisfies {import('./$types').Actions} */
+import { request, gql, GraphQLClient } from 'graphql-request';
+
+// import TEST from './catalog/[...slug]/mutations.js';
+const urlCRUD = import.meta.env.VITE_URL;
+const key = import.meta.env.VITE_KEY;
+
+const graphQLClient = new GraphQLClient(urlCRUD, {
+	headers: {
+		Authorization: `Bearer ${key}`,
+	},
+});
+
+
+
+
 export const actions = {
 	ggg2: async () => {
-		console.log('ggg2');
-		return { success: true };
+
+
+
+
+
+
+	
+		const TEST = gql`
+	mutation create_product($id: UUID!, $key: UUID!, $value: String!, $slug: String) {
+		createProduct(input: { id: $id, key: $key, value: $value, slug: $slug }) {
+			id
+			key
+			value
+			slug
+		}
+			}
+		`;
+
+		const variables = {
+			id: "9eb340c2-9795-4d70-a081-aa1a0ec94a84",
+			key: "9eb340c2-9795-4d70-a081-aa1a0ec94a84",
+			value: "тестовое значение2",
+			slug: "etoslug2"
+		};
+
+		const req = await graphQLClient.request(TEST, variables);
+		console.log(req);
+
+		// return { success: true };
 	}
 };
