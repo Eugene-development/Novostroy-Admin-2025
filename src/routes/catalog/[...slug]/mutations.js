db709bdbe8b5289ca3d566379fef420c47d269a4
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-15 19:40:39 +0300

    c

--- src/routes/catalog/[...slug]/mutations.js
+++ src/routes/catalog/[...slug]/mutations.js
@@ -1,10 +1,12 @@
 import { gql } from 'graphql-request';
 
 const mutation = gql`
-	mutation create_product($id: UUID!, $key: UUID!, $value: String!) {
-		createProduct(input: { id: $id, key: $key, value: $value }) {
+	mutation create_product($id: UUID!, $key: UUID!, $value: String!, $slug: String) {
+		createProduct(input: { id: $id, key: $key, value: $value, slug: $slug }) {
 			id
+			key
 			value
+			slug
 		}
 	}
 `;
