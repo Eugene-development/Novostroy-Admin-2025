c0f6887bc35451a6622f91c13ed8f53c56340eaa
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-17 18:12:43 +0300

    c

--- src/routes/catalog/modals/ProductFormCreate/index.svelte
+++ src/routes/catalog/modals/ProductFormCreate/index.svelte
@@ -38,13 +38,33 @@
 </script>
 
 {#if visibleProductFormCreate.value}
-	<div
-		class="fixed inset-0 z-50 flex items-center overflow-y-auto justify-center p-4"
-		aria-labelledby="modal-title"
-		role="dialog"
-		aria-modal="true"
-	>
-		<div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+
+
+<div class="relative mt-8 z-50" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+	<!--
+	  Background backdrop, show/hide based on modal state.
+  
+	  Entering: "ease-out duration-300"
+		From: "opacity-0"
+		To: "opacity-100"
+	  Leaving: "ease-in duration-200"
+		From: "opacity-100"
+		To: "opacity-0"
+	-->
+	<div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+  
+	<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+	  <div class="flex min-h-full items-end justify-center p-4  sm:items-center sm:p-0">
+		<!--
+		  Modal panel, show/hide based on modal state.
+  
+		  Entering: "ease-out duration-300"
+			From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+			To: "opacity-100 translate-y-0 sm:scale-100"
+		  Leaving: "ease-in duration-200"
+			From: "opacity-100 translate-y-0 sm:scale-100"
+			To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+		-->
 		<div class="relative w-full max-w-5xl rounded-lg bg-white shadow sm:p-5 dark:bg-gray-800">
 			<!-- Modal header -->
 			<div
@@ -484,5 +504,8 @@
 				</div>
 			</form>
 		</div>
+	  </div>
 	</div>
-{/if}
+  </div>
+  {/if}
+
