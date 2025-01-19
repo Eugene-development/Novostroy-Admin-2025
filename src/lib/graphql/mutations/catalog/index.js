ded1f8559688585821f06733ebe844e3843622fa
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-19 21:10:41 +0300

    c

--- src/lib/graphql/mutations/catalog/index.js
+++ src/lib/graphql/mutations/catalog/index.js
@@ -8,7 +8,8 @@ export const CREATE_PRODUCT = gql`
 		$slug: String
 		$parentable_type: String
 		$parentable_id: UUID
-	) {
+		$images_hash: [CreateImageInput!]!
+		) {
 		createProduct(
 			input: {
 				id: $id
@@ -17,13 +18,40 @@ export const CREATE_PRODUCT = gql`
 				slug: $slug
 				parentable_type: $parentable_type
 				parentable_id: $parentable_id
+				image: { create: $images_hash }
 			}
 		) {
 			id
-			key
-			value
-			slug
-			# parentable
 		}
 	}
 `;
+
+
+
+// export const CREATE_PRODUCT = gql`
+// 	mutation create_product(
+// 		$id: UUID!
+// 		$key: UUID!
+// 		$value: String!
+// 		$slug: String
+// 		$parentable_type: String
+// 		$parentable_id: UUID
+// 	) {
+// 		createProduct(
+// 			input: {
+// 				id: $id
+// 				key: $key
+// 				value: $value
+// 				slug: $slug
+// 				parentable_type: $parentable_type
+// 				parentable_id: $parentable_id
+// 			}
+// 		) {
+// 			id
+// 			key
+// 			value
+// 			slug
+// 			# parentable
+// 		}
+// 	}
+// `;
